929c219d8a393d245ebc431fc12dd9857f8a3c9f
Matthew Viego <mjv35@pitt.edu>
2019-06-03 21:39:33 -0400

    changed message

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -5,7 +5,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
     return (
         <div>
             <p className='f3'>
-                {'Detect faces in your pictures. Give it a try!'}
+                {'Detect faces in your pictures. Paste an image link below!'}
             </p>
             <div className='center'>
                 <div className='form center pa4 br3 shadow-5'>
